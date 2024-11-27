86f7761206f36c26c4b047115654ca6c568cc3ed
neerp03 <157861342+neerp03@users.noreply.github.com>
2024-11-27 17:18:07 -0500

    Update executor.c

--- Project#3/executor.c
+++ Project#3/executor.c
@@ -1,90 +1,104 @@
-#include "executor.h"
-#include "builtin.h"
-#include "utility.h"
+#include "mysh.h"
 
-void execute_command(Command *cmd) {
-    if (is_builtin(cmd->name)) {
-        execute_builtin(cmd);
+/**
+ * Executes parsed commands.
+ * This file handles both built-in commands and external program executions.
+ */
+
+// Executes a single command (either built-in or external)
+void run_command(command_t *cmd) {
+    if (is_builtin_command(cmd->program)) {
+        execute_builtin_command(cmd);
     } else {
-        execute_external(cmd);
+        execute_external_program(cmd);
     }
 }
 
-void execute_builtin(Command *cmd) {
-    if (strcmp(cmd->name, "cd") == 0) {
-        cmd_cd(cmd->args);
-    } else if (strcmp(cmd->name, "pwd") == 0) {
-        cmd_pwd();
-    } else if (strcmp(cmd->name, "which") == 0) {
-        cmd_which(cmd->args);
-    } else if (strcmp(cmd->name, "exit") == 0) {
-        cmd_exit(cmd->args);
+// Handles the execution of built-in commands
+void execute_builtin_command(command_t *cmd) {
+    if (strcmp(cmd->program, "cd") == 0) {
+        change_directory(cmd->arguments);
+    } else if (strcmp(cmd->program, "pwd") == 0) {
+        print_working_directory();
+    } else if (strcmp(cmd->program, "which") == 0) {
+        find_program_location(cmd->arguments);
+    } else if (strcmp(cmd->program, "exit") == 0) {
+        terminate_shell(cmd->arguments);
     }
 }
 
-void execute_external(Command *cmd) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        error_handler("Fork failed");
-        return;
-    }
-    if (pid == 0) {  // Child process
-        handle_redirection(cmd);
-        execv(cmd->path, cmd->args);
-        error_handler("Execution failed");
+// Executes an external program
+void execute_external_program(command_t *cmd) {
+    pid_t child_pid = fork();
+
+    if (child_pid == 0) {
+        setup_redirections(cmd);
+        execv(cmd->program, cmd->arguments);
+        perror("Execution failed");
         exit(EXIT_FAILURE);
-    } else {  // Parent process
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            int code = WEXITSTATUS(status);
-            if (code != 0) {
-                printf("Command failed with code %d\n", code);
-            }
-        } else if (WIFSIGNALED(status)) {
-            printf("Terminated by signal: %d\n", WTERMSIG(status));
-        }
+    } else if (child_pid > 0) {
+        int exit_status;
+        waitpid(child_pid, &exit_status, 0);
+        handle_exit_status(exit_status);
+    } else {
+        perror("Forking failed");
     }
 }
 
-void handle_redirection(Command *cmd) {
+// Configures file redirections using dup2
+void setup_redirections(command_t *cmd) {
     if (cmd->input_file) {
-        int fd = open(cmd->input_file, O_RDONLY);
-        if (fd < 0) error_handler("Failed to open input file");
-        dup2(fd, STDIN_FILENO);
-        close(fd);
+        int in_fd = open(cmd->input_file, O_RDONLY);
+        if (in_fd < 0) {
+            perror("Failed to open input file");
+            exit(EXIT_FAILURE);
+        }
+        dup2(in_fd, STDIN_FILENO);
+        close(in_fd);
     }
+
     if (cmd->output_file) {
-        int fd = open(cmd->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0640);
-        if (fd < 0) error_handler("Failed to open output file");
-        dup2(fd, STDOUT_FILENO);
-        close(fd);
+        int out_fd = open(cmd->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+        if (out_fd < 0) {
+            perror("Failed to open output file");
+            exit(EXIT_FAILURE);
+        }
+        dup2(out_fd, STDOUT_FILENO);
+        close(out_fd);
     }
 }
 
-void handle_pipe(Command *cmd1, Command *cmd2) {
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        error_handler("Pipe failed");
+// Configures pipelines between two commands
+void setup_pipeline(command_t *cmd1, command_t *cmd2) {
+    int pipe_fd[2];
+
+    if (pipe(pipe_fd) == -1) {
+        perror("Pipe creation failed");
         return;
     }
-    pid_t pid1 = fork();
-    if (pid1 == 0) {  // First child
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[0]);
-        execv(cmd1->path, cmd1->args);
-        exit(EXIT_FAILURE);
-    }
-    pid_t pid2 = fork();
-    if (pid2 == 0) {  // Second child
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[1]);
-        execv(cmd2->path, cmd2->args);
-        exit(EXIT_FAILURE);
-    }
-    close(pipefd[0]);
-    close(pipefd[1]);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-}
 
+    pid_t first_child = fork();
+    if (first_child == 0) {
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        execv(cmd1->program, cmd1->arguments);
+        perror("Execution failed");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t second_child = fork();
+    if (second_child == 0) {
+        dup2(pipe_fd[0], STDIN_FILENO);
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        execv(cmd2->program, cmd2->arguments);
+        perror("Execution failed");
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+    waitpid(first_child, NULL, 0);
+    waitpid(second_child, NULL, 0);
+}
